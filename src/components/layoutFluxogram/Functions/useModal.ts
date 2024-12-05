16d9db8e41148dfa3acdf3092b180673a5e3870c
Pedro Leuchs <pedro.leuchs@stwautomacao.com.br>
2024-12-05 15:25:35 -0300

    fix: code refactored

---
+++ src/components/layoutFluxogram/Functions/useModal.ts
@@ -0,0 +1,76 @@
+import { Node } from '@xyflow/react';
+import { useState, useCallback } from 'react';
+
+interface ModalProps {
+  onShowAlert: (
+    message: string,
+    severity: 'error' | 'warning' | 'info' | 'success'
+  ) => void;
+  setNodeEditing: (node: Node) => void;
+}
+
+export const useModal = ({ onShowAlert, setNodeEditing }: ModalProps) => {
+  const [modalEdgeOpen, setmodalEdgeOpen] = useState(false);
+  const [watingNode, setWatingNode] = useState(false);
+  const [modalCircle, setModalCircle] = useState(false);
+  const [modalPhase, setModalPhase] = useState(false);
+  const [modalSquare, setModalSquare] = useState(false);
+  const [modalSeparator, setModalSeparator] = useState(false);
+  const [modalUnity, setModalUnity] = useState(false);
+  const [modalLabel, setModalLabel] = useState(false);
+
+  const handleWaitingClickOnNode = () => {
+    onShowAlert('Clique no nó que deseja editar.', 'info');
+    setWatingNode(true);
+  };
+
+  const openModalEditNode = useCallback(
+    (node: Node) => {
+      setNodeEditing(node);
+
+      const nodeType = node.type ? node.type : '';
+
+      if (nodeType === 'circle') {
+        setModalCircle(true);
+      }
+      if (nodeType === 'square') {
+        setModalSquare(true);
+      }
+      if (nodeType === 'phase') {
+        setModalPhase(true);
+      }
+      if (nodeType === 'unity') {
+        setModalUnity(true);
+      }
+      if (nodeType === 'label') {
+        setModalLabel(true);
+      }
+      if (nodeType === 'separator') {
+        setModalSeparator(true);
+      }
+
+      setWatingNode(false);
+    },
+    [setWatingNode, setNodeEditing, setModalCircle]
+  );
+
+  return {
+    modalEdgeOpen,
+    watingNode,
+    modalCircle,
+    modalLabel,
+    modalPhase,
+    modalSeparator,
+    modalSquare,
+    modalUnity,
+    setmodalEdgeOpen,
+    setModalCircle,
+    setModalLabel,
+    setModalPhase,
+    setModalSeparator,
+    setModalSquare,
+    setModalUnity,
+    handleWaitingClickOnNode,
+    openModalEditNode,
+  };
+};
