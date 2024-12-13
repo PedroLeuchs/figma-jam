d455da578a3a66a18246acfddc637905ea521b38
Pedro Leuchs <pedro.leuchs@stwautomacao.com.br>
2024-12-13 08:22:44 -0300

    feature styled download modal

--- src/components/modal/ModalDownload.tsx
+++ src/components/modal/ModalDownload.tsx
@@ -1,69 +1,71 @@
-import { useState } from "react";
-import * as Dialog from "@radix-ui/react-dialog";
-import { FaDownload } from "react-icons/fa6";
+import { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { FaDownload } from 'react-icons/fa6';
 import { toPng, toSvg } from 'html-to-image';
 import {
-  useReactFlow, 
+  useReactFlow,
   getNodesBounds,
   getViewportForBounds,
-} from "@xyflow/react";
-import * as ToggleGroup from "@radix-ui/react-toggle-group";
-import TextInput from "../textInput/TextInput";
+} from '@xyflow/react';
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import TextInput from '../textInput/TextInput';
 
 export const ModalDownload = () => {
   const { getNodes } = useReactFlow();
   const [imageWidth, setImageWidth] = useState(1024);
   const [imageHeight, setImageHeight] = useState(768);
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedRatio, setSelectedRatio] = useState("16:9");
-const [text, setText] = useState('diagram')
-const [format, setFormat] = useState<'png' | 'svg'>('png');
+  const [selectedRatio, setSelectedRatio] = useState('16:9');
+  const [text, setText] = useState('diagram');
+  const [format, setFormat] = useState<'png' | 'svg'>('png');
 
-    const handleDownload = () => {
-        const nodesBounds = getNodesBounds(getNodes());
-        const viewport = getViewportForBounds(
-          nodesBounds,
-          imageWidth,
-          imageHeight,
-          0.01,
-          2.5,
-          0.1
-        );
-    
-        const viewportElement = document.querySelector('.react-flow__viewport') as HTMLElement | null;
-        if (viewportElement) {
-          const options = {
-            backgroundColor: '#ffffff',
-            width: imageWidth,
-            height: imageHeight,
-            style: {
-              width: `${imageWidth}px`,
-              height: `${imageHeight}px`,
-              transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
-            },
-          };
-    
-          if (format === 'png') {
-            toPng(viewportElement, options)
-              .then((dataUrl) => {
-                const link = document.createElement('a');
-                link.download = `${text}.png`;
-                link.href = dataUrl;
-                link.click();
-              })
-              .catch((err) => console.error('Erro ao exportar imagem PNG:', err));
-          } else if (format === 'svg') {
-            toSvg(viewportElement, options)
-              .then((dataUrl) => {
-                const link = document.createElement('a');
-                link.download = `${text}.svg`;
-                link.href = dataUrl;
-                link.click();
-              })
-              .catch((err) => console.error('Erro ao exportar imagem SVG:', err));
-          }
-        }
+  const handleDownload = () => {
+    const nodesBounds = getNodesBounds(getNodes());
+    const viewport = getViewportForBounds(
+      nodesBounds,
+      imageWidth,
+      imageHeight,
+      0.01,
+      2.5,
+      0.1
+    );
+
+    const viewportElement = document.querySelector(
+      '.react-flow__viewport'
+    ) as HTMLElement | null;
+    if (viewportElement) {
+      const options = {
+        backgroundColor: '#ffffff',
+        width: imageWidth,
+        height: imageHeight,
+        style: {
+          width: `${imageWidth}px`,
+          height: `${imageHeight}px`,
+          transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
+        },
       };
+
+      if (format === 'png') {
+        toPng(viewportElement, options)
+          .then((dataUrl) => {
+            const link = document.createElement('a');
+            link.download = `${text}.png`;
+            link.href = dataUrl;
+            link.click();
+          })
+          .catch((err) => console.error('Erro ao exportar imagem PNG:', err));
+      } else if (format === 'svg') {
+        toSvg(viewportElement, options)
+          .then((dataUrl) => {
+            const link = document.createElement('a');
+            link.download = `${text}.svg`;
+            link.href = dataUrl;
+            link.click();
+          })
+          .catch((err) => console.error('Erro ao exportar imagem SVG:', err));
+      }
+    }
+  };
 
   const onCancel = () => {
     setIsOpen(false);
@@ -77,13 +79,13 @@ const [format, setFormat] = useState<'png' | 'svg'>('png');
   const handleToggleChange = (value: string) => {
     setSelectedRatio(value);
 
-    if (value === "16:9") {
+    if (value === '16:9') {
       setImageWidth(1280);
       setImageHeight(720);
-    } else if (value === "4:3") {
+    } else if (value === '4:3') {
       setImageWidth(1024);
       setImageHeight(768);
-    } else if (value === "9:16") {
+    } else if (value === '9:16') {
       setImageWidth(720);
       setImageHeight(1280);
     }
@@ -105,7 +107,11 @@ const [format, setFormat] = useState<'png' | 'svg'>('png');
           <Dialog.Title>Configurações de Download</Dialog.Title>
           {/* Seleção de Proporção */}
           <div className="w-full h-[40%] gap-5 flex flex-col items-start justify-start">
-          <TextInput onChange={setText} value={text} titleInput={'Nome do arquivo'}/>
+            <TextInput
+              onChange={setText}
+              value={text}
+              titleInput={'Nome do arquivo'}
+            />
             <ToggleGroup.Root
               className="inline-flex bg-gray-300 rounded shadow-md w-full h-full border border-gray-500"
               type="single"
@@ -118,21 +124,27 @@ const [format, setFormat] = useState<'png' | 'svg'>('png');
                 value="16:9"
                 aria-label="16:9"
               >
-                <div className="w-[64%] h-[36%] bg-white border border-gray-500"></div>
+                <div className="w-[64%] h-[36%] bg-white border border-gray-500 flex items-center justify-center">
+                  <p className="text-2xl text-black">16:9</p>
+                </div>
               </ToggleGroup.Item>
               <ToggleGroup.Item
                 className="bg-white text-black h-full w-1/3 flex items-center justify-center text-sm font-normal first:rounded-l last:rounded-r hover:bg-gray-200 focus:outline-none data-[state=on]:bg-violet-400 data-[state=on]:text-white"
                 value="4:3"
                 aria-label="4:3"
               >
-                <div className="w-[40%] h-[30%] bg-white border border-gray-500"></div>
+                <div className="w-[40%] h-[30%] bg-white border border-gray-500 flex items-center justify-center">
+                  <p className="text-2xl text-black">4:3</p>
+                </div>
               </ToggleGroup.Item>
               <ToggleGroup.Item
                 className="bg-white text-black h-full w-1/3 flex items-center justify-center text-sm font-normal first:rounded-l last:rounded-r hover:bg-gray-200 focus:outline-none data-[state=on]:bg-violet-400 data-[state=on]:text-white"
                 value="9:16"
                 aria-label="9:16"
               >
-                <div className="w-[36%] h-[64%] bg-white border border-gray-500"></div>
+                <div className="w-[36%] h-[64%] bg-white border border-gray-500 flex items-center justify-center">
+                  <p className="text-2xl text-black">9:16</p>
+                </div>
               </ToggleGroup.Item>
             </ToggleGroup.Root>
             <ToggleGroup.Root
